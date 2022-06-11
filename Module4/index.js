084a5a14deb4260b2d19450834139e98d4f92d75
Marshall <103530652+onethickmom@users.noreply.github.com>
2022-06-11 17:17:52 -0500

    Add files via upload
    Updated for class COP1801. 3 functions are created compared to 1 per instructions. Original function is commented out on bottom of index.js

--- Module4/index.js
+++ Module4/index.js
@@ -1,30 +1,81 @@
-function calculate() {
+var serviceText
+var service
+var total
+var tipAmount
 
-    let serviceText = prompt("Please rate your service; great, ok, or poor").toLowerCase();
+//Service information, how was service and recommended tip amount in decimal form
+serviceFunction = () =>  {
+    serviceText = prompt("Please rate your service; great, ok, or poor").toLowerCase();
 
-    /*convert service into recommended tip percentage */
-    if (serviceText == "great") {
+        if (serviceText == "great") {
         service = 0.20
     } else if(serviceText == "ok") {
         service = 0.15
     } else if(serviceText == "poor") {
         service = 0.10
-    } else {
+        } else {
+        service = 0.00
         window.alert('Please select a service option provided')
         return
     }
-    
-    let total = prompt("Please enter your amount between $5 and $500")
+}
 
-    if (total >= 5 && total <= 500) {
-        /*calculate recommended tip with message */
-        let tipAmount = total * service
+//Total amount, how much and ensuring it's a valid amount
+totalFunction = () => {
 
-        /*display results*/
-        document.getElementById("results").innerHTML = "Your total was " + total + ". The recommended tip for " + serviceText + " service is $" + tipAmount + "."
-    } else {
-        
-        window.alert("Please enter a valid amount.")
+    if (service == 0.00) {
         return
+    } else {
+        total = prompt("Please enter your total bill, between 5-500")
+            if (total < 5 && total > 500) {
+                window.alert("Please enter a valid amount.")
+                return
+            }
+    }
+
+}
+
+
+//Calculate the tip amount and display recommended message
+calculate = () =>  {
+    if (service == 0.00) {
+        return
+    } else {
+        tipAmount = total * service
+        document.getElementById("results").innerHTML = "Your total was $" + total + ". The recommended tip for " + serviceText + " service is $" + tipAmount.toFixed(2) + "."
     }
 }
+
+// function calculate() {
+
+//     let serviceText = prompt("Please rate your service; great, ok, or poor").toLowerCase();
+
+//     /*convert service into recommended tip percentage */
+//     if (serviceText == "great") {
+//         service = 0.20
+//     } else if(serviceText == "ok") {
+//         service = 0.15
+//     } else if(serviceText == "poor") {
+//         service = 0.10
+//     } else {
+//         window.alert('Please select a service option provided')
+//         return
+//     }
+    
+//     let total = prompt("Please enter your amount between $5 and $500")
+
+//     if (total >= 5 && total <= 500) {
+//         /*calculate recommended tip with message */
+//         let tipAmount = total * service
+
+//         /*display results*/
+//         document.getElementById("results").innerHTML = "Your total was " + total + ". The recommended tip for " + serviceText + " service is $" + tipAmount + "."
+//     } else {
+        
+//         window.alert("Please enter a valid amount.")
+//         return
+//     }
+// }
+
+
+
